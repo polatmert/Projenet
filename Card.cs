2bd6fcb4f41c17928d88e52774df859acfaa2ee7
polatmert <husnumertpolatt@gmail.com>
2019-03-29 02:06:12 +0300

    Added script code to run
    Buttons controls and Test data added.
    Added script code to run.

--- Card.cs
+++ Card.cs
@@ -27,14 +27,21 @@ namespace Projenet
 
         private void btnReset_Click(object sender, EventArgs e)
         {
-            Clear();
+            try
+            {
+                Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }       
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             try
             {
-                if(!string.IsNullOrWhiteSpace(txtCode.Text))
+                if (!string.IsNullOrWhiteSpace(txtCode.Text))
                 {
                     SqlConnection card = DatabaseConnection.ConnectionDB();
                     SqlCommand sqlcmd = new SqlCommand("ItemAdd", card);
@@ -49,7 +56,7 @@ namespace Projenet
                     Clear();
                     List(card);
                     card.Close();
-                }  
+                }
                 else
                 {
                     MessageBox.Show("please enter the code");
@@ -88,15 +95,21 @@ namespace Projenet
             {
                 MessageBox.Show(ex.Message);
             }
-
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            SqlConnection con = DatabaseConnection.ConnectionDB();
-            Delete(con);
-            Clear();
-            List(con);
+            try
+            {
+                SqlConnection con = DatabaseConnection.ConnectionDB();
+                Delete(con);
+                Clear();
+                List(con);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnChoose_Click(object sender, EventArgs e)
@@ -143,46 +156,84 @@ namespace Projenet
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            try
+            {
+                Application.Exit();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
         }
 
         private void dataGridView1_RowHeaderMouseClick_1(object sender, DataGridViewCellMouseEventArgs e)
         {
-            Id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            try
+            {
+                Id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
         }
 
         public void List(SqlConnection con)
         {
-            SqlDataAdapter sqlDataAdaper = new SqlDataAdapter("Select * from Items", con);
-            DataTable dataTable = new DataTable();
-            sqlDataAdaper.Fill(dataTable);
-            dataGridView1.DataSource = dataTable;
-            con.Close();
+            try
+            {
+                SqlDataAdapter sqlDataAdaper = new SqlDataAdapter("Select * from Items", con);
+                DataTable dataTable = new DataTable();
+                sqlDataAdaper.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
         }
 
         public void Clear()
         {
-            txtName.Text = txtSurname.Text = txtCity.Text = txtCode.Text = txtpath.Text = "";
+            try
+            {
+                txtName.Text = txtSurname.Text = txtCity.Text = txtCode.Text = txtpath.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
         }
 
         public void Update(SqlConnection con)
         {
             try
             {
-                SqlCommand sqlcmd;
-                sqlcmd = new SqlCommand("update Items set Name=@name,Surname=@Surname,Code=@Code,City=@City,State=@State,Date=@Date where Id=@Id", con);
-                sqlcmd.Parameters.AddWithValue("@Id", Id);
-                sqlcmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
-                sqlcmd.Parameters.AddWithValue("@Surname", txtSurname.Text.Trim());
-                sqlcmd.Parameters.AddWithValue("@Code", txtCode.Text.Trim());
-                sqlcmd.Parameters.AddWithValue("@City", txtCity.Text.Trim());
-                sqlcmd.Parameters.AddWithValue("@State", chBoxActive.Checked);
-                sqlcmd.Parameters.AddWithValue("@Date", dateTimePicker1.Value);
+                if (Id != 0)
+                {
+                    SqlCommand sqlcmd;
+                    sqlcmd = new SqlCommand("update Items set Name=@name,Surname=@Surname,Code=@Code,City=@City,State=@State,Date=@Date where Id=@Id", con);
+                    sqlcmd.Parameters.AddWithValue("@Id", Id);
+                    sqlcmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Surname", txtSurname.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Code", txtCode.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@City", txtCity.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@State", chBoxActive.Checked);
+                    sqlcmd.Parameters.AddWithValue("@Date", dateTimePicker1.Value);
 
-                sqlcmd.ExecuteNonQuery();
-                MessageBox.Show("Record Updated Successfully");
+                    sqlcmd.ExecuteNonQuery();
+                    MessageBox.Show("Record Updated Successfully");
+
+                }
+                else
+                {
+                    MessageBox.Show("Please select the item");
+                }
+
                 con.Close();
-
             }
             catch (Exception ex)
             {
@@ -200,6 +251,10 @@ namespace Projenet
                     cmd.Parameters.AddWithValue("@Id", Id);
                     cmd.ExecuteNonQuery();
                 }
+                else
+                {
+                    MessageBox.Show("Please select the item");
+                }
             }
             catch (Exception ex)
             {
@@ -209,31 +264,38 @@ namespace Projenet
 
         public void InserDataToDB(dynamic data, int rowCount, int columnCount)
         {
-            SqlConnection card = DatabaseConnection.ConnectionDB();
-            int i = 2;
-            while (i <= rowCount)
+            try
             {
-                SqlCommand sqlcmd = new SqlCommand("ItemAdd", card);
-                sqlcmd.CommandType = CommandType.StoredProcedure;
-                sqlcmd.Parameters.AddWithValue("@Name", data[i, 1]);
-                sqlcmd.Parameters.AddWithValue("@Surname", data[i, 2]);
-                if (string.IsNullOrWhiteSpace(data[i, 3]))
+                SqlConnection card = DatabaseConnection.ConnectionDB();
+                int i = 2;
+                while (i <= rowCount)
                 {
-                    MessageBox.Show(i + " item code is incorrect or empty");
-                    break;
-                }
-                    
-                sqlcmd.Parameters.AddWithValue("@Code", data[i, 3]);
-                sqlcmd.Parameters.AddWithValue("@City", data[i, 4]);
-                DateTime newdatetime = Convert.ToDateTime(data[i, 5]);
-                sqlcmd.Parameters.AddWithValue("@Date", newdatetime);
-                if (data[i, 6] == 1)
-                    sqlcmd.Parameters.AddWithValue("@State", true);
-                else
-                    sqlcmd.Parameters.AddWithValue("@State", false);
+                    SqlCommand sqlcmd = new SqlCommand("ItemAdd", card);
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    sqlcmd.Parameters.AddWithValue("@Name", data[i, 1]);
+                    sqlcmd.Parameters.AddWithValue("@Surname", data[i, 2]);
+                    if (string.IsNullOrWhiteSpace(data[i, 3]))
+                    {
+                        MessageBox.Show(i + " item code is incorrect or empty");
+                        break;
+                    }
 
-                sqlcmd.ExecuteNonQuery();
-                i++;
+                    sqlcmd.Parameters.AddWithValue("@Code", data[i, 3]);
+                    sqlcmd.Parameters.AddWithValue("@City", data[i, 4]);
+                    DateTime newdatetime = Convert.ToDateTime(data[i, 5]);
+                    sqlcmd.Parameters.AddWithValue("@Date", newdatetime);
+                    if (data[i, 6] == 1)
+                        sqlcmd.Parameters.AddWithValue("@State", true);
+                    else
+                        sqlcmd.Parameters.AddWithValue("@State", false);
+
+                    sqlcmd.ExecuteNonQuery();
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);               
             }
         }
     }
